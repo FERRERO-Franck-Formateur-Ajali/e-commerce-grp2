c50f33e2d28950f4b5ee70960a07f081076dc809
trochaudpj <pjtggl@gmail.com>
2021-06-29 16:28:49 +0200

    first

---
+++ src/Controller/ProduitsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produits;
+use App\Repository\ProduitsRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitsController extends AbstractController
+{
+    /**
+     * @Route("/produits", name="produits")
+     */
+    public function index(ProduitsRepository $produitsRepository): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Produits::class);
+        $produits= $repo->findall();
+        
+        return $this->render('produits/index.html.twig', [
+            'produits' => $produits,
+            'controller_name' => 'ProduitsController',
+        ]);
+    }
+}
